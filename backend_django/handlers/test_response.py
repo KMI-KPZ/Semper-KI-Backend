d809d3809f4aa815a1f1ec91ec4e987ee82c3869
SWIAI <weging@infai.org>
2022-11-10 12:42:17 +0100

    unsecure stuff

--- backend_django/handlers/test_response.py
+++ backend_django/handlers/test_response.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+
 # this makes it possible to assume for a function, that certain requests are passed through
 from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET", "POST"]) # get and post will make it this far
+@csrf_exempt # ONLY FOR TESTING!!!!
 def test_response(request):
     if request.method == "GET":
         return HttpResponse("Hello, world. GET")
