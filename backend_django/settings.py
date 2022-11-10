6ccc61ae9ec1939cad3c0be4db15032319c0e2c5
SWIAI <weging@infai.org>
2022-11-10 13:39:56 +0100

    cors!

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000","http://127.0.0.1:3000",]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000","http://127.0.0.1:3000",]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
 # Application definition
 
@@ -37,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
