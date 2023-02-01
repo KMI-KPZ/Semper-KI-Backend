b4c75e9af10f9c669b367aa42ae297805b354bdf
SWIAI <weging@infai.org>
2023-02-01 16:39:57 +0100

    mockup for getData

--- backend_django/handlers/filter.py
+++ backend_django/handlers/filter.py
@@ -12,5 +12,20 @@ def getData(request):
     :rtype: JSON
 
     """
-    # ID, name, file(picture)
-    return JsonResponse({})
+    # get filters set by user
+    filters = json.loads(request.body.decode("utf-8"))
+    # Filter name is in question -> title, selected stuff is in "answer"
+    filtersForSparql = []
+    for entry in filters["filters"]:
+        filtersForSparql.append([entry["question"]["title"], entry["answer"]])
+    #TODO ask via sparql with most general filter and then iteratively filter response
+    
+    # mockup here:
+    models = {"models": []}
+    for i in range(20):
+        models["models"].append(
+            {"name": "testmodel", "tags": "test, test, test", "date": "2023-02-01", "license": "MIT", "certificate": "None", "URI": "https://i0.wp.com/letsprint3d.net/wp-content/uploads/2017/04/benchy.jpg?resize=663%2C373&ssl=1"}
+        )
+    filters.update(models)
+    
+    return JsonResponse(filters)
