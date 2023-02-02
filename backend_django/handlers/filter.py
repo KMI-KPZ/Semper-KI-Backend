4f47f123eed0903f1201d910ee1455d6f33ab2f6
SWIAI <weging@infai.org>
2023-02-02 13:50:52 +0100

    getData update

--- backend_django/handlers/filter.py
+++ backend_django/handlers/filter.py
@@ -24,7 +24,7 @@ def getData(request):
     models = {"models": []}
     for i in range(20):
         models["models"].append(
-            {"name": "testmodel", "tags": "test, test, test", "date": "2023-02-01", "license": "MIT", "certificate": "None", "URI": "https://i0.wp.com/letsprint3d.net/wp-content/uploads/2017/04/benchy.jpg?resize=663%2C373&ssl=1"}
+            {"name": "testmodel", "tags": ["test", "test", "test"], "date": "2023-02-01", "license": "MIT", "certificate": ["None"], "URI": "https://i0.wp.com/letsprint3d.net/wp-content/uploads/2017/04/benchy.jpg?resize=663%2C373&ssl=1"}
         )
     filters.update(models)
     
