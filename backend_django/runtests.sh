297dc3fcbc191aa308a45625644e3dffa9214d86
SWIAI <weging@infai.org>
2023-05-17 12:52:17 +0200

    runtests environment added

--- backend_django/runtests.sh
+++ backend_django/runtests.sh
@@ -4,4 +4,5 @@ pwd
 gunicorn --bind 0.0.0.0:8000 backend_django.asgi --reload --env DJANGO_SETTINGS_MODULE=backend_django.settings.production --capture-output --log-level debug -k uvicorn.workers.UvicornWorker --workers 16 --threads 16 &
 sleep 5
 cd backend_django
+export DJANGO_SETTINGS_MODULE=backend_django.settings.production
 python ../manage.py test 
