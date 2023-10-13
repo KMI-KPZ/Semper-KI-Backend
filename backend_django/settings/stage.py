462e8b2a1b06f913378507f412e2873abb2d340b
ThomasSkodawessely <skodawessely@infai.org>
2023-10-13 14:44:14 +0200

    show current settings used settings file

--- backend_django/settings/stage.py
+++ backend_django/settings/stage.py
@@ -14,21 +14,29 @@ from .base import *
 
 DEBUG = False
 DEVELOPMENT=True
-BACKEND_SETTINGS= "development"
+BACKEND_SETTINGS= "stage"
 
 # for nginx
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_DOMAIN = '.semper-ki.org'
 
-
-REDIS_HOST = "files-dev"
-CELERY_BROKER_URL = "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0"
-CELERY_RESULT_BACKEND = "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0"
+CELERY_BROKER_URL = CELERY_RESULT_BACKEND=  f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0'
+print("CELERY_BROKER_URL: "+CELERY_BROKER_URL)
 
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0",
+        "LOCATION": f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0",
     }
 }
+
+# CELERY_BROKER_URL = "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0"
+# CELERY_RESULT_BACKEND = "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0"
+#
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": "redis://:"+REDIS_PASSWORD+"@files-dev:6379/0",
+#     }
+# }
