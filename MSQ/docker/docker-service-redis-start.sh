967a31f42cb8f32e1bc9878bdc445f5b822b74a0
Akshay NS <shewatkar@infai.org>
2023-12-14 15:14:19 +0100

    sh script for redisbroker

--- MSQ/docker/docker-service-redis-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-<<<<<<<< HEAD:MSQ/docker/dc-service-redis-start.sh
-echo "Starting Redis"
-exec docker-entrypoint.sh --save 60 1 --loglevel warning --requirepass $REDISPW
-========
-exec docker-entrypoint.sh --save 60 1 --loglevel warning --requirepass $REDIS_PASSWORD
->>>>>>>> dev:MSQ/docker/docker-service-redis-start.sh
