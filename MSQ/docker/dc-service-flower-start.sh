90966c4a7cd4be49e94bef590f955dd9626a3994
Akshay NS <shewatkar@infai.org>
2023-12-14 13:19:40 +0100

    load redis port from .env

--- MSQ/docker/dc-service-flower-start.sh
+++ MSQ/docker/dc-service-flower-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /data
 echo "Starting Flower with REDIS_HOST=${REDIS_HOST} REDIS_PORT=${REDIS_PORT} REDISPW=${REDISPW}"
-exec celery --broker=redis://redis-broker:6379/0 flower --port=8888
+exec celery --broker=redis://redis-broker:${REDIS_PORT}/0 flower --port=8888
 exec docker network connect semperki-local-dev_semper-ki-dev-network flower
 
