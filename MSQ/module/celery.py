bfd6820a97cecce2e450de6c6fa837722d1dc04b
Akshay NS <shewatkar@infai.org>
2024-02-14 16:44:08 +0100

    added MSQ folder containing celery

---
+++ MSQ/module/celery.py
@@ -0,0 +1,29 @@
+"""
+Part of Semper-KI software
+
+Akshay NS 2023
+
+Contains: Configuration for Celery worker
+
+"""
+from __future__ import absolute_import
+
+from dotenv import load_dotenv
+
+from celery import Celery, shared_task
+
+import os
+############################################
+
+load_dotenv('./MSQ/.env')
+
+broker_url = os.getenv('CELERY_BROKER_URL')
+backend_url = os.getenv('CELERY_BACKEND_URL')
+
+app = Celery('module',
+             broker=broker_url,
+             backend=backend_url,
+             )
+
+if __name__ == '__main__':
+    app.start()
