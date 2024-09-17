57da4e9b6f108ca5ded185d838738d38fe468fc9
LukasHeinInfai <hein@infai.org>
2024-09-17 11:17:05 +0200

    Added first Additional Service Skeletons
    Service Enumeration (for now)
    
    AdditiveManufacturing()
    SERVICE_NAME = "ADDITIVE_MANUFACTURING"
    SERVICE_NUMBER = 1
    
    CreateModel()
    SERVICE_NAME = "CREATE_MODEL"
    SERVICE_NUMBER = 2
    
    NEW:
    
    Delivery()
    SERVICE_NAME = "DELIVERY"
    SERVICE_NUMBER = 3
    
    PostProcessing()
    SERVICE_NAME = "POST-PROCESSING"
    SERVICE_NUMBER = 4
    
    Packaging()
    SERVICE_NAME = "PACKAGING"
    SERVICE_NUMBER = 5
    
    QualityControl()
    SERVICE_NAME = "QUALITY_CONTROL"
    SERVICE_NUMBER = 6
    
    AfterSales()
    SERVICE_NAME = "AFTER-SALES"
    SERVICE_NUMBER = 7
    
    Assembly()
    SERVICE_NAME = "ASSEMBLY"
    SERVICE_NUMBER = 8

---
+++ code_SemperKI/services/service_Post-Processing/service.py
@@ -0,0 +1,83 @@
+"""
+Part of Semper-KI software
+
+Silvio Weging 2023
+
+Contains: Class which describes the service in particular
+"""
+import code_SemperKI.serviceManager as Semper
+
+from .connections.postgresql.pgService import updateServiceDetails as CM_updateServiceDetails, deleteServiceDetails as CM_deleteServiceDetails, serviceReady as CM_serviceReady, cloneServiceDetails as CM_cloneServiceDetails
+from .handlers.checkService import checkIfSelectionIsAvailable as CM_checkIfSelectionIsAvailable
+
+###################################################
+class PostProcessing(Semper.ServiceBase):
+    """
+    All connections of this service that Semper-KI should know about
+    
+    """
+    ###################################################
+    def __init__(self) -> None:
+        super().__init__()
+
+    ###################################################
+    def updateServiceDetails(self, existingContent, newContent):
+        """
+        Run service specific update of service details
+
+        """
+
+        return CM_updateServiceDetails(existingContent, newContent)
+    
+    ###################################################
+    def deleteServiceDetails(self, existingContent, newContent):
+        """
+        Run service specific update of service details
+
+        """
+
+        return CM_deleteServiceDetails(existingContent, newContent)
+    
+    ###################################################
+    def serviceReady(self, existingContent) -> bool:
+        """
+        Checks, if service is completely defined
+        
+        """
+        return CM_serviceReady(existingContent)
+    
+    ###################################################
+    def checkIfSelectionIsAvailable(self, processObj) -> bool:
+        """
+        Checks, if the selection of the service is available (material, ...)
+        
+        """
+        return CM_checkIfSelectionIsAvailable(processObj)
+    
+    ####################################################################################
+    def cloneServiceDetails(self, existingContent:dict, newProcess) -> dict:
+        """
+        Clone content of the service
+
+        :param existingContent: What the process currently holds about the service
+        :type existingContent: dict
+        :param newProcess: The new process as object
+        :type newProcess: Process|ProcessInterface
+        :return: The copy of the service details
+        :rtype: dict
+        
+        """
+        return CM_cloneServiceDetails(existingContent, newProcess)
+
+    ###################################################
+    def getFilteredContractors(self, processObj) -> list:
+        """
+        Get a list of contractors that can do the job
+
+        """
+        return []
+
+SERVICE_NAME = "POST-PROCESSING"
+SERVICE_NUMBER = 4
+
+Semper.serviceManager.register(SERVICE_NAME, SERVICE_NUMBER, PostProcessing())
