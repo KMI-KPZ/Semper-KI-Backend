c055d1fc11beec6d74d06165014dbfa5c0761743
LukasHeinInfai <hein@infai.org>
2025-02-12 17:16:53 +0100

    CreateModel Skeleton added updated

--- code_SemperKI/services/service_CreateModel/definitions.py
+++ code_SemperKI/services/service_CreateModel/definitions.py
@@ -7,14 +7,36 @@ Contains: Definitions, Classes, Enums to describe Elements in the Backend as wel
 """
 
 ##################################################
-# What makes up the 3D print service
+# What makes up the Create Model service
 import enum
 
 from Generic_Backend.code_General.utilities.customStrEnum import StrEnumExactlyAsDefined
 
+##################################################
+# Service specific definitions
+SERVICE_NAME = "CREATE_MODEL"
+SERVICE_NUMBER = 2
+
+###################################################
 class ServiceDetails(StrEnumExactlyAsDefined):
     """
     What does the service consists of 
 
     """
-    pass
+
+####################################################
+class NodeTypesCM(StrEnumExactlyAsDefined):
+    """
+    What possible types can a node have in the CM service?
+    
+    """
+    organization = enum.auto() # the orga node
+
+##################################################
+class OrganizationDetailsCM(StrEnumExactlyAsDefined):
+    """
+    What are the details of an organization?
+    
+    """
+
+##################################################
