566e1c76f7a742838d1896fe144bbbea356588fd
LukasHeinInfai <hein@infai.org>
2025-02-12 17:19:21 +0100

    Post Processing Skeleton added updated

--- code_SemperKI/services/service_PostProcessing/definitions.py
+++ code_SemperKI/services/service_PostProcessing/definitions.py
@@ -7,14 +7,36 @@ Contains: Definitions, Classes, Enums to describe Elements in the Backend as wel
 """
 
 ##################################################
-# What makes up the 3D print service
+# What makes up the Post Processing service
 import enum
 
 from Generic_Backend.code_General.utilities.customStrEnum import StrEnumExactlyAsDefined
 
+##################################################
+# Service specific definitions
+SERVICE_NAME = "POST_PROCESSING"
+SERVICE_NUMBER = 4
+
+###################################################
 class ServiceDetails(StrEnumExactlyAsDefined):
     """
     What does the service consists of 
 
     """
-    pass
+
+####################################################
+class NodeTypesPP(StrEnumExactlyAsDefined):
+    """
+    What possible types can a node have in the PP service?
+    
+    """
+    organization = enum.auto() # the orga node
+
+##################################################
+class OrganizationDetailsPP(StrEnumExactlyAsDefined):
+    """
+    What are the details of an organization?
+    
+    """
+
+##################################################
