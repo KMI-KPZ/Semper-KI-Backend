179587814b2af03bd5a213112fefb66f717bed35
SWIAI <weging@infai.org>
2025-03-10 16:42:25 +0100

    temporary directory is a class now

--- code_SemperKI/services/service_AdditiveManufacturing/utilities/stpToStl.py
+++ code_SemperKI/services/service_AdditiveManufacturing/utilities/stpToStl.py
@@ -14,6 +14,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from Generic_Backend.code_General.definitions import FileObjectContent
 from Generic_Backend.code_General.utilities.basics import checkIfNestedKeyExists
+from Generic_Backend.code_General.utilities.temporaryFolder import temporaryDirectory
 
 loggerError = logging.getLogger("errors")
 ##################################################
@@ -30,22 +31,19 @@ def transformSTPtoSTL(stpFile:InMemoryUploadedFile, fileName:str) -> BytesIO|Exc
     
     """
     try:
+        fileName = fileName.split(".")[0] # remove the stp ending
+        temporaryFileName = temporaryDirectory.createTemporaryFile(fileName+".stp", stpFile.read())
+        cqStpfile = cq.importers.importStep(temporaryFileName)
+        #assembly = cq.Assembly(cqStpfile)
+        #cq.assembly.Assembly.save(assembly, temporaryOutFileName)
         stlContent = BytesIO()
-        with tempfile.TemporaryDirectory() as tempDir:
-            temporaryFileName = tempDir+"\\"+fileName
-            fileName = fileName.split(".")[0] # remove the stp ending
-            temporaryOutFileName = tempDir+"\\"+fileName+".stl"
-            temporaryFile = open(temporaryFileName, 'wb')
-            temporaryFile.write(stpFile.read())
-            temporaryFile.close()
-            cqStpfile = cq.importers.importStep(temporaryFileName)
-            #assembly = cq.Assembly(cqStpfile)
-            #cq.assembly.Assembly.save(assembly, temporaryOutFileName)
-            exporters.export(cqStpfile, temporaryOutFileName)
-            with open(temporaryOutFileName, 'rb') as stlFile:
-                stlContent.write(stlFile.read())
-            stlContent.seek(0)
-
+        tempDir = temporaryDirectory.getTemporaryFolderPath()
+        temporaryOutFileName = tempDir+"/"+fileName+".stl"
+        exporters.export(cqStpfile, temporaryOutFileName)
+        with open(temporaryOutFileName, 'rb') as stlFile:
+            stlContent.write(stlFile.read())
+        stlContent.seek(0)
+        temporaryDirectory.deleteTemporaryFile(fileName+".stp")
         return stlContent
     except Exception as error:
         loggerError.error(f"Error while transforming STP to STL: {str(error)}")
